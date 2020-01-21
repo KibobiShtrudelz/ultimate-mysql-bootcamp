f51ebc7323b626a0fc48466a4373e35e2b80b3e3
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2020-01-21 10:20:00 +0200

    311. CODE: Connecting the Form - finished

--- join-us-project/app.js
+++ join-us-project/app.js
@@ -1,38 +1,51 @@
 const express = require('express');
 const mysql = require('mysql');
-const bodyParser  = require("body-parser");
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  database : 'join_us'
+	host: 'localhost',
+	user: 'root',
+	database: 'join_us'
 });
 
-app.get("/", function(req, res){
-    // Find count of users in DB
-    const q = "SELECT COUNT(*) AS count FROM users";
-    connection.query(q, function(err, results){
-        if(err) throw err;
-        const count = results[0].count;
-        res.render("home", {count: count});
-    });
+app.get('/', function(req, res) {
+	const q = 'SELECT COUNT(*) as count FROM users';
+
+	connection.query(q, function(error, results) {
+		if (error) throw error;
+
+		res.render('home', { data: results[0].count });
+	});
 });
 
-app.post("/register", function(req, res){
-    const person = {
-        email: req.body.email
-    };
-    connection.query('INSERT INTO users SET ?', person, function(err, result) {
-        if (err) throw err;
-        res.redirect("/");
-    });
+app.get('/joke', (req, res) => {
+	const joke = '<strong>Wa? <em>-Za!</em></strong>';
+	res.send(joke);
 });
 
-app.listen(8080, function(){
-    console.log("Server running on 8080!");
+app.get('/register', (req, res) => {
+	res.send('reg get');
+});
+
+app.post('/register', (req, res) => {
+	const person = { email: req.body.email };
+	connection.query('INSERT INTO users SET ?', person, (error, results) => {
+		if (error) throw error;
+		console.log(results);
+		res.redirect('/');
+	});
+});
+
+app.get('/random_num', (req, res) => {
+	const num = Math.floor(Math.random() * 10 + 1);
+	res.send('Your lucky number is ' + num);
+});
+
+app.listen(3000, () => {
+	console.log('App listening on port 3000!');
 });
